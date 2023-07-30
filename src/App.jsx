e0ef467b044b4b676da8f77b8c597718906d0904
Luis Rodríguez <lfrz.arc@gmail.com>
2023-07-30 04:42:40 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
-import './styles/app.css'
-import { Canvas } from '@react-three/fiber'
-import { Sky } from '@react-three/drei'
-import { Physics } from '@react-three/cannon'
-import { Ground } from './components/Ground'
-import { FPV } from './components/FPV'
-import { Model } from './components/Model'
-import { Venus } from './components/Venus'
-import { useState } from 'react'
+import "./styles/app.css";
+import { Canvas } from "@react-three/fiber";
+import { Sky } from "@react-three/drei";
+import { Physics } from "@react-three/cannon";
+import { Ground } from "./components/Ground";
+import { FPV } from "./components/FPV";
+import { Model } from "./components/Model";
+import { Venus } from "./components/Venus";
+import { useState } from "react";
 
-function App () {
-  const [hidden, setHidden] = useState(true)
+function App() {
+  const [hidden, setHidden] = useState(true);
   function showName() {
-    console.log('showed')
-    setHidden(!hidden)
+    console.log("showed");
+    setHidden(!hidden);
   }
 
-  return(
+  return (
     <>
-      <div className='display' style={{display: hidden ? 'none' : 'inline'}}>Planet name: <span>Xolotl (Venus)</span></div>
+      <div className="display" style={{ display: hidden ? "none" : "inline" }}>
+        Planet name: <span>Xolotl (Venus)</span>
+      </div>
       <Canvas>
         <Sky sunPosition={[1, 0, 0]} />
         <ambientLight intensity={0.5} />
         <directionalLight color={"white"} position={[10, 10, 0]} />
         <FPV />
         <Physics>
-          <Venus  showName={showName} />
+          <Venus showName={showName} />
           <Model url={"./src/3D_models/TemploMayor.stl"} />
           <Ground />
         </Physics>
       </Canvas>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
