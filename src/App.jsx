624b875116f56a6b8faf46c21e1a94ad6b012035
Luis Rodríguez <lfrz.arc@gmail.com>
2023-07-31 12:29:35 -0600

    Testing event.touches to add controls for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -6,35 +6,63 @@ import { Ground } from './components/Ground'
 import { FPV } from './components/FPV'
 import { Model } from './components/Model'
 import { Venus } from './components/Venus'
-import { useState } from 'react'
-import { WebXRController, XRHandSpace } from 'three'
+import { useRef, useState } from 'react'
+
 
 function App () {
   const [hidden, setHidden] = useState(true)
+  const [touchStart, setTouchStart] = useState({x:0, y:0})
+  const [movement, setMovement] = useState({x:0, y:0})
+  const cameraRef = useRef(null)
+
+  function handleTouchStart(e) {
+    setTouchStart(
+      {
+        x: e.touches[0].clientX,
+        y: e.touches[0].clientY
+      }
+    )
+  }
+  function handleTouchMove(e) {
+    const newX = e.touches[0].clientX
+    const newY = e.touches[0].clientY
+
+    const deltaX = newX - touchStart.x
+    const deltaY = newY - touchStart.y
+
+    setMovement(
+      {
+        x: deltaX * 0.01,
+        y: deltaY * 0.01
+      }
+    )
+
+    //console.log(`X: ${(deltaX * 0.01)} Y:${(deltaY * 0.01)}`)
+  }
+
   function showName() {
     console.log('showed')
     setHidden(!hidden)
   }
 
+
   return(
     <>
       <div className='display' style={{display: hidden ? 'none' : 'inline'}}>Planet name: <span>Xolotl (Venus)</span></div>
-      <Canvas>
+      <Canvas
+        onTouchStart={(e) => handleTouchStart(e)}
+        onTouchMove={(e) => handleTouchMove(e)}
+      >
         <Sky sunPosition={[1, 0, 0]} />
         <ambientLight intensity={0.5} />
         <directionalLight color={"white"} position={[10, 10, 0]} />
-        <FPV />
+        <FPV cameraRef={cameraRef} movement={movement} />
         <Physics>
           <Venus  showName={showName} />
           <Model url={"./src/3D_models/TemploMayor.stl"} />
           <Ground />
         </Physics>
       </Canvas>
-      <WebXRController>
-        <mesh visible={false} />
-      </WebXRController>
-      <XRHandSpace hand="left" />
-      <XRHandSpace hand="right" />
     </>
   )
 }
--- src/components/FPV.jsx
+++ src/components/FPV.jsx
@@ -1,12 +1,16 @@
-import { PointerLockControls } from "@react-three/drei";
-import { useThree } from "@react-three/fiber";
+import { PerspectiveCamera, PointerLockControls } from "@react-three/drei";
+import { Canvas, useThree } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import { Vector3 } from "three";
 
-export function FPV () {
+export function FPV ({ cameraRef, movement }) {
   const { camera, gl } = useThree()
 
+  useThree(({camera}) => {
+    camera.rotation.set(-(movement.y), -(movement.x), 0)
+  })
+
   return (
-    <PointerLockControls
-      args={[camera, gl.domElement]}
-    />
+    <PointerLockControls args={[camera, gl]} />
   )
 }
