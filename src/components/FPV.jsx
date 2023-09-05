519c143adcf4ef202fbb5d59ff2f926b0f74910b
Luis Rodríguez <lfrz.arc@gmail.com>
2023-09-04 22:59:28 -0600

    Camera rotation fixed

--- src/components/FPV.jsx
+++ src/components/FPV.jsx
@@ -1,16 +1,23 @@
 import { PerspectiveCamera, PointerLockControls } from "@react-three/drei";
 import { Canvas, useThree } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
-import { Vector3 } from "three";
+import { Quaternion, Vector3 } from "three";
 
 export function FPV ({ cameraRef, movement }) {
   const { camera, gl } = useThree()
 
   useThree(({camera}) => {
-    const x = movement.x
-    const y = movement.y
-
-    camera.rotation.set(y, x, 0)
+    const targetRotation = new Quaternion()
+    const currentRotation = new Quaternion()
+    const X = new Quaternion().setFromAxisAngle(new Vector3(1, 0, 0), movement.y);
+    const Y = new Quaternion().setFromAxisAngle(new Vector3(0, 1, 0), movement.x);
+  
+    // Combina las rotaciones y aplica a la cámara
+    targetRotation.multiplyQuaternions(Y, X);
+    camera.quaternion.copy(targetRotation.clone());
+  
+    // Actualiza la rotación actual
+    currentRotation.copy(camera.quaternion);
   })
 
   return (
