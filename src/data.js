f5b3494cbe1725bef09c4d255468dda9f991a6f3
Luis Rodríguez <lfrz.arc@gmail.com>
2024-10-13 22:39:44 -0600

    I added tonatiuh cualo s thumbnail, also compressed and added the rest of the 3d models

--- src/data.js
+++ src/data.js
@@ -33,7 +33,7 @@ export const data = [
   },
   {
     id: crypto.randomUUID(),
-    modelName: '',
+    modelName: 'venus_3d',
     nameNa: 'Xolotl',
     nameEs: 'Venus',
     description: 'Las representaciones iconográficas de Venus son diversas; incluso, en el Templo Rojo de Cacaxtla se representó de forma humanizada, y en la lámina 58 del Códice Dresde la cabeza de la deidad descendente se plasmó por medio de una cruz o estrella.\n Si deseas saber más sobre la compleja iconografía de Venus te invito a leer el siguiente artículo: Representaciones de venus en el méxico prehispanico',
@@ -48,7 +48,7 @@ export const data = [
   },
   {
     id: crypto.randomUUID(),
-    modelName: '',
+    modelName: 'citlalin_popoca_3d',
     nameNa: 'Citlalin popoca',
     nameEs: 'Cometa',
     description: 'Citlalin popoca significa literalmente "estrella que humea", aquí está representa con el glifo de citlali que en conjugación con unas bolutas de humo alude a la idea de un cometa',
@@ -63,7 +63,7 @@ export const data = [
   },
   {
     id: crypto.randomUUID(),
-    modelName: '',
+    modelName: 'citlalin_tlamina_3d',
     nameNa: 'Citlalin tlamina',
     nameEs: 'Citlalin tlamina significa literalmente "estrella fugaz", aquí se representa con el glifo de citlali que en conjugación del glifo tlamina "flecha" alude a la idea de una estrella fugaz',
     description: '',
@@ -78,7 +78,7 @@ export const data = [
   },
   {
     id: crypto.randomUUID(),
-    modelName: '',
+    modelName: 'yohualli_3d',
     nameNa: 'Yohualli',
     nameEs: 'La noche',
     description: '',
@@ -102,13 +102,13 @@ export const data = [
     scale: 1,
     images: {
       thumbnail: {
-        src: ''
+        src: 'https://i.ibb.co/Sdr5rMf/tonatiuh-cualo.webp'
       }
     }
   },
   {
     id: crypto.randomUUID(),
-    modelName: '',
+    modelName: 'meztli_cualo_3d',
     nameNa: 'Meztli cualo',
     nameEs: 'Eclipse lunar',
     description: 'Este glifo está compuesto por dos pictogramas: Yohualli "la noche" (parte izq.) y Meztli "la luna" (parte derecha) pues la traducción interpretada de "Meztli cualo" es "La oscuridad devoró a la luna".',
