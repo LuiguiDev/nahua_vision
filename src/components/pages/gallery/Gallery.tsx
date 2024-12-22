7aa6c687ed4a5506089e984445849c3c086bb0d9
Luis Rodríguez <lfrz.arc@gmail.com>
2024-12-21 23:11:52 -0600

    added a default styles to buttons, color palette stablished

--- src/components/pages/gallery/Gallery.tsx
+++ src/components/pages/gallery/Gallery.tsx
@@ -2,11 +2,39 @@ import React from 'react'
 import './gallery.css'
 import ImagePack from './ImagePack'
 
+const TrashButton = () => {
+  return(
+    <button className='trash_btn'>
+      🚮
+    </button>
+  )
+}
+
+interface DownloadButtonProps {
+  numArticles: number
+}
+
+const DownloadButton: React.FC <DownloadButtonProps> = ({ numArticles }) => {
+  const isDisabled = numArticles > 0 ? false : true
+  return(
+    <button
+      className={`download_btn ${isDisabled ? 'disabled' : ''}`}
+      disabled={isDisabled}
+    >
+      Descargar ({numArticles})
+    </button>
+  )
+}
+
 const Gallery = () => {
   return(
     <main className="page_container">
       <h3>Descarga las imagenes de Nahua Vision adaptadas para fondo de pantalle GRATIS</h3>
-      <ImagePack cover='./../img/cover.jpg' title='Fondos de pantalla con etiqueta' />
+
+      <div className="options">
+        <TrashButton />
+        <DownloadButton numArticles={1} />
+      </div>
     </main>
   )
 }
