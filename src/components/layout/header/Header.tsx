080bd57199671995306b377af0e9741e7b56a35a
Luis Rodríguez <lfrz.arc@gmail.com>
2025-01-23 17:57:16 -0600

    trying helmet to add img and description when sharing the url

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -33,7 +33,6 @@ export const Header = () => {
 
   return (
     <header>
-      <img src="./public/img/cover.png" style={{position: 'fixed', top: '-300px'}} /> 
       <Link to={'/'}>
         <img src="https://i.ibb.co/HY4Jgph/favicon.png" alt="nahua vision logo" className="logo" />
       </Link>
